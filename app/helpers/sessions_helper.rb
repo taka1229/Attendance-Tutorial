964421cac5e6624d0dbdee9074ee6d96dcd346b6
taka <okiro.taka555@gmail.com>
2020-04-29 08:21:53 +0000

    8.2完了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,13 +5,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # 永続的セッションを記憶します（Userモデルを参照）
+   # 永続的セッションを記憶します（Userモデルを参照）
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
+
+  # 永続的セッションを破棄します
   def forget(user)
     user.forget # Userモデル参照
     cookies.delete(:user_id)
@@ -37,6 +38,11 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+  def current_user?(user)
+    user == current_user
   end
 
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
