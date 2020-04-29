7de9a32f1b49395a65a1386aa6b9a034fb9d9fab
taka <okiro.taka555@gmail.com>
2020-04-29 07:42:14 +0000

    8.1.1完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,18 @@ class UsersController < ApplicationController
   end
 
   def create
-  @user = User.new(user_params)
-  if @user.save
-    log_in @user # 保存成功後、ログインします。
-    flash[:success] = '新規作成に成功しました。'
-    redirect_to @user
-  else
-    render :new
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
   end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
